e1e078ce338ed0a18700a00956f941f4c3ae5397
Ted Brookings <ted.brookings@googlemail.com>
2014-09-28 12:23:29 -0400

    Improved speed of pythagorean, updated 039.py

--- 039.py
+++ 039.py
@@ -1,36 +1,20 @@
 #!/usr/bin/python
 
 
-from pythagorean import genPythagoreanWithPerimeter, genPythagorean
-import sys
-if sys.version_info[0] == 2:
-  # get rid of 2.x range that produced list instead of iterator
-  range = xrange
+from pythagorean import genPythagoreanWithPerimeter
 
-
-def numTripletsWithPerimeter(perimeter):
-  return sum(1 for triplet in genPythagoreanWithPerimeter(perimeter))
-
-def euler39Old(maxPerimeter=1000):
-  maxNum, maxP = max(
-    ((numTripletsWithPerimeter(p), p) for p in range(12, maxPerimeter + 1)),
-    key=lambda t:t[0]
-  )
-  print('For perimeter <= %d, the maximum number of pythagorean triplets '
-        'occurs with p == %d (%d triplets)' % (maxPerimeter, maxP, maxNum))
 
 def euler39(maxPerimeter=1000):
   sums =  [0 for p in range(1 + maxPerimeter)]
-  for trip in genPythagorean(maxC=maxPerimeter):
-    p = sum(trip)
-    if p <= maxPerimeter:
-      sums[p] += 1
+  for trip in genPythagoreanWithPerimeter(maxPerimeter=maxPerimeter):
+    perimeter = trip[-1]
+    sums[perimeter] += 1
   maxP, maxNum = max(((p, s) for p, s in enumerate(sums)), key=lambda t: t[1])
   print('For perimeter <= %d, the maximum number of pythagorean triplets '
         'occurs with p == %d (%d triplets)' % (maxPerimeter, maxP, maxNum))
 
 
 if __name__ == "__main__":
+  import sys
   args = tuple(eval(a) for a in sys.argv[1:])
   euler39(*args)
-  #euler39Old(*args)
--- pythagorean.py
+++ pythagorean.py
@@ -5,26 +5,27 @@ import sys
 if sys.version_info[0] == 2:
   # get rid of 2.x range that produced list instead of iterator
   range = xrange
-from math import sqrt
-
+from math import sqrt, ceil
+#from blist import sortedlist
+from collections import deque
 
 def isPythagorean(a, b, c):
   # return True if a^2 + b^2 == c^2, False otherwise
   return a*a + b*b == c*c
 
 
-def genPythagorean(maxC=float('inf'), maxB=None, primitive=False):
+def genPythagorean(maxC=float('inf'), maxB=None, genDerived=True):
   # generate all Pythagorean triples, such that c <= maxC, b <= maxB
   #   if primitive=True, then only generates triples with no common factors
   if maxB is None:
     maxB = maxC
-  computeDerived = not primitive
   
   m, n = 2, 1
   # add primitive seed for (3,4,5)
-  seeds = [(2,1)]
+  seeds = deque([(2,1)])
+  
   while seeds:
-    tup = seeds.pop(0)
+    tup = seeds.popleft()
     if len(tup) == 2:
       # this m,n tuple describes a primitive pythagorean triple
       m,n = tup
@@ -34,7 +35,7 @@ def genPythagorean(maxC=float('inf'), maxB=None, primitive=False):
       if c > maxC:
         continue
       b = 2*m*n
-      if c > maxB:
+      if b > maxB:
         continue
       a = mSqrd-nSqrd
       if a > b:
@@ -45,7 +46,7 @@ def genPythagorean(maxC=float('inf'), maxB=None, primitive=False):
       seeds.append((2*m - n, m))
       seeds.append((2*m + n, m))
       seeds.append((m + 2*n, n))
-      if computeDerived:
+      if genDerived:
         # add a derived seed
         seeds.append((a,b,c, 2))
     else:
@@ -65,59 +66,68 @@ def genPythagorean(maxC=float('inf'), maxB=None, primitive=False):
       seeds.append((a,b,c, k+1))
 
 
-def genPythagoreanSlow(maxC=float('inf'), maxB=None, primitive=False):
+def genPythagoreanSlow(maxC=float('inf')):
   # generates all Pythagorean triples. Slow, for testing purposes.
   from math import sqrt
   c = 2
   while c <= maxC:
-    for a in range(1, int(c/sqrt(2.0)) + 1):
-      b = int(round(sqrt(c*c - a*a)))
-      
+    for b in range(int(ceil(c/sqrt(2.0))), c):
+      a = int(round(sqrt(c*c - b*b)))
       if isPythagorean(a, b, c):
         yield a,b,c
     c += 1
 
 
-def genPythagoreanWithPerimeter(perimeter, primitive=False):
+def genPythagoreanWithPerimeter(maxPerimeter=float('inf'), genDerived=True):
   # generate all Pythagorean triples, such that c <= maxC, b <= maxB
   #   if primitive=True, then only generates triples with no common factors
-  computeDerived = not primitive
-  
-  maxN = int(round(sqrt(perimeter / 4)))
-  perimStop = perimeter / 2
-  
   m, n = 2, 1
   # add primitive seed for (3,4,5)
-  seeds = [(2,1)]
+  seeds = deque([(2,1)])
+  
   while seeds:
-    tup = seeds.pop(0)
-    # this m,n tuple describes a primitive pythagorean triple
-    m,n = tup
-    mSqrd, nSqrd = m*m, n*n
-    a, b, c = mSqrd-nSqrd, 2*m*n, mSqrd + nSqrd
-    if a > b:
-      a, b = b, a
-    p = a + b + c
-    
-    if p >= perimeter:
-      if p == perimeter:
-        # found a match, yield it
-        yield a, b, c
-      # this tree is too big, will never have matching descendents
-      continue
-    
-    if computeDerived:
-      # Not a match, but maybe a multiplicative factor will match
-      f, r = divmod(perimeter, p)
-      if r == 0:
-        # f * (a,b,c) is a match
-        yield (f * a, f * b, f * c)
+    tup = seeds.popleft()
+    if len(tup) == 2:
+      # this m,n tuple describes a primitive pythagorean triple
+      m,n = tup
+      # calculate pythagorean triple
+      mSqrd, nSqrd = m*m, n*n
+      a, b, c = mSqrd-nSqrd, 2*m*n, mSqrd + nSqrd
+      p = a + b + c
+      if p > maxPerimeter:
+        # perimeter too large to yield or produce derived triplets
+        continue
       
-    # add three new primitive seeds
-    if m <= maxN:
+      if a > b:
+        a, b = b, a
+      yield a, b, c, p
+      
+      if p == maxPerimeter:
+        # perimeter too big to produce derived triplets
+        continue
+
+      # add three new primitive seeds
       seeds.append((2*m - n, m))
       seeds.append((2*m + n, m))
-    seeds.append((m + 2*n, n))
+      seeds.append((m + 2*n, n))
+      if genDerived:
+        # add a derived seed
+        seeds.append((a,b,c, p, 2))
+    else:
+      # this a,b,c,k tuple describes a primitive pythagorean triple and
+      # a multiplying factor
+      a,b,c, p, k = tup
+      pk = p * k
+      if pk > maxPerimeter:
+        continue
+      # yield derived pythagorean triple
+      yield a*k, b*k, c*k, pk
+      
+      if pk == maxPerimeter:
+        continue
+      
+      # add next derived seed
+      seeds.append((a,b,c, p, k+1))
 
   
 def testAssert(booleanVal, message):
@@ -146,6 +156,7 @@ def testPythagorean(maxC=1000):
                 'genPythagorean generated ' + str(trip) +
                 ' which is not a pythagorean triplet'
               )
+  # assert there are no repeats
   testAssert( len(set(generatedTrips)) == len(generatedTrips),
               'genPythagorean generated duplicate triplets'
             )
@@ -179,31 +190,31 @@ def testPythagorean(maxC=1000):
                 )
     raise
 
-  # test fix perimeter generation
-  for p in range(12, maxC):
-    fixedTrips = sorted(genPythagoreanWithPerimeter(p),
-                        key=lambda t:t[2]-1.0/t[1])
-    testAssert( len(set(fixedTrips)) == len(fixedTrips),
-               'genPythagoreanWithPerimeter generated duplicate triplets'
-            )
-    genTrips = sorted((t for t in generatedTrips if sum(t) == p),
+  # test perimeter-based generation
+  perimTrips = sorted(genPythagoreanWithPerimeter(maxC),
                       key=lambda t:t[2]-1.0/t[1])
-    try:
-      assert fixedTrips == genTrips
-    except AssertionError:
-      for trip in fixedTrips:
-        # assert each generated triple is valid
-        testAssert( isPythagorean(*trip),
-                    'genPythagoreanWithPerimeter generated ' + str(trip) +
-                    ' which is not a pythagorean triplet'
-                  )
-      for trip in genTrips:
-        # assert fixed-perimeter triple was comprehensive
-        testAssert( trip in fixedTrips,
-                    'genPythagoreanWithPerimeter failed to generate '
-                    + str(trip) + ' with perimeter %d' % p
-                  )
-      raise
+  for trip in perimTrips:
+    # assert each generated triple is valid
+    a, b, c, p = trip
+    testAssert( isPythagorean(a,b,c),
+                'genPythagoreanWithPerimeter generated ' + str(trip[0:3]) +
+                ' which is not a pythagorean triplet'
+              )
+    testAssert( a + b + c == p,
+                'genPythagoreanWithPerimeter generated perimeter that doesn''t'
+                ' match triplet'
+              )
+  # assert there are no repeats
+  testAssert( len(set(perimTrips)) == len(perimTrips),
+              'genPythagoreanWithPerimeter generated duplicate triplets'
+            )
+  for trip in generatedTrips:
+    # assert perimTrips is comprehensive
+    p = sum(trip)
+    if p < maxC:
+      testAssert( trip + (p,) in perimTrips,
+                  'genPythagoreanWithPerimeter failed to generate ' + str(trip)
+                )
     
   print(' passed')
 
